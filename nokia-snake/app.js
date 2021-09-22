c7e97167845c04284061132ac614028f5c42d8ff
pyrhaa <sboubtane@protonmail.com>
2021-09-22 18:23:19 +0200

    display queries

---
+++ nokia-snake/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div');
+  const scoreDisplay = document.querySelector('span');
+  const startBtn = document.querySelector('.start');
+});
